c0c1ed51ca88064d9a1466e6b6cfbeb76b330aea
arsenii-152 <arseniytnt@gmail.com>
2022-03-28 16:35:55 +0300

    API folder added

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,19 @@
 Rails.application.routes.draw do
 
+  namespace :api do
+    namespace :v1 do
+      # get 'posts/:id/comments', to: 'comments#index', as: 'post_comments'
+      # post 'posts/:id/comments/create', to: 'comments#create', as: 'create_post_comment'
+      # put 'comments/:id', to: 'comments#update', as: 'update_post_comment'
+    end
+  end
+
   resources :quizzes do
     resources :questions do
       resources :answers
     end
   end
+
 
 
   get 'answers', to: 'answers#index'
