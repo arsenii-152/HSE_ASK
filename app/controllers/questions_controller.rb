1bd6cd689549495daa159ca1a5d335571eef1eef
jaroslaw <jaroslaw.chaikin@gmail.com>
2022-03-29 23:35:26 +0300

    create forms on react, and API:Create

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -12,5 +12,11 @@ class QuestionsController < ApplicationController
 		@quiz = Quiz.find(params[:quiz_id])
 		@question = @quiz.questions.create(params[:question].permit(:title))
 		redirect_to quiz_path(@quiz)
-	end
+  end
+  def destroy
+    @quiz = Quiz.find(params[:quiz_id])
+    @question = @quiz.questions.find(params[:id])
+    @question.destroy
+    redirect_to quiz_path(@quiz)
+  end
 end
